784dc920809325390a577af3c23c9fdc188eef72
Cody Chase <cchase1991@gmail.com>
2024-04-25 10:35:55 -0400

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import './index.css';
+import ReadmeGen from './pages/readmeGen.jsx';
+
+function App() {
+	return (
+		<>
+			<ReadmeGen />
+		</>
+	);
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'purple-c': '#6e57a5',
+        'blue-light': '#92daf1',
+      }
+    },
+  },
+  plugins: [],
+}
+
